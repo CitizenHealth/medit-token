7ddd4ff5deb31a422a2115075d0a4593d412e6bd
James Cunningham <j.a.cunningham@gmail.com>
2019-01-09 11:55:30 +0000

    Update truffle-config to use config.json

--- src/truffle-config.js
+++ src/truffle-config.js
@@ -23,11 +23,7 @@
  */
 
 const HDWalletProvider = require('truffle-hdwallet-provider');
-const infuraKey = "6044524c9c914ef48641e4e2783c4240";
-//
-const fs = require('fs');
-const mnemonic = fs.readFileSync(".secret").toString().trim();
-const account = 2;
+const config = require('./config.json')
 
 module.exports = {
   /**
@@ -66,7 +62,7 @@ module.exports = {
     // Useful for deploying to a public network.
     // NB: It's important to wrap the provider as a function.
     ropsten: {
-      provider: () => new HDWalletProvider(mnemonic, `https://ropsten.infura.io/${infuraKey}`, account),
+      provider: () => new HDWalletProvider(config.mnemonic, `https://ropsten.infura.io/${config.infuraKey}`, config.account),
       network_id: 3,       // Ropsten's id
       gas: 5500000,        // Ropsten has a lower block limit than mainnet
       confirmations: 2,    // # of confs to wait between deployments. (default: 0)
